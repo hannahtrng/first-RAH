768b1af578c076ac07a7fccab16718b9a20eb87d
Hannah Truong <truonghannah1@gmail.com>
2024-10-21 19:42:23 -0700

    hw3 update
    changing fish to collectables, added obstacle course

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float boostModifier = 2f;
+    [SerializeField] Animator animator;
     Rigidbody2D rb;
     Vector2 inputDirection = Vector2.zero;
 
@@ -15,19 +16,29 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
 
     }
+    void OnMove2(InputValue value)
+    {
+        Vector2 movementDir = value.Get<Vector2>();
+        Vector2 boostDirection = movementDir.normalized;
+        rb.AddForce(boostDirection * 25, ForceMode2D.Impulse);
+    }
     void OnMove(InputValue value)
     {
         Vector2 movementDir = value.Get<Vector2>();
         Debug.Log(movementDir);
 
         //rb.AddForce(movementDir * boostModifier, ForceMode2D.Impulse);
+        rb.velocity = movementDir * 3;
         inputDirection = movementDir;
-    }
-    void OnMove2(InputValue value)
-    {
-        Vector2 movementDir = value.Get<Vector2>();
-        Vector2 boostDirection = movementDir.normalized;
-        rb.AddForce(boostDirection * 25, ForceMode2D.Impulse);
+        if (Mathf.Approximately(rb.velocity.magnitude, 0))
+        {
+            animator.SetBool("running", false);
+        }
+        else
+        {
+            animator.SetBool("running", true);
+        }
+
     }
 
     private void Update()
